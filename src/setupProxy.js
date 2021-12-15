31b841badd7ef1f0108c26bcca268416a6e734fe
Lok Jian Ming <jianminglok@hotmail.com>
2021-12-16 00:25:01 +0800

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,11 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = app => {
-  app.use(proxy("/api/*", { target: "https://nushalls-backend.jianminglok.gq/" }));
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'https://nushalls-backend.jianminglok.gq/',
+            changeOrigin: true
+        })
+    );
 };
